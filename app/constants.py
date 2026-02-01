a7c888917e4bbe581aeebcb09190a36439551980
Ravi Vardiparti <vvravi.1311@gmail.com>
2026-02-01 10:38:55 -0500

    rest server working with a dummy graph

---
+++ app/constants.py
@@ -0,0 +1,9 @@
+
+# agent nodes
+PLANNER_AGENT = "planner_agent"
+
+
+
+# model used for the agents
+MEDICARE_MODEL="gpt-3.5-turbo"
+RAG_EMBEDDING_MODEL="text-embedding-3-small"
---
+++ app/server.py
@@ -0,0 +1,22 @@
+from pprint import pprint
+
+from fastapi import FastAPI
+
+# project imports
+from .medicare_agent_graph import run_medicare_agent
+from .server_models import InvokeRequest
+
+
+app = FastAPI(title="Medicare Agent", version="1.0.0")
+
+
+@app.get("/health")
+def health():
+    return {"status": "okk"}
+@app.post("/medicare-agent")
+def medicare_agent(request: InvokeRequest):
+    # config = {"configurable": {"thread_id": request.thread_id or "default"}}
+    result = run_medicare_agent(request)
+    # final = result["messages"][-1].content
+    pprint(result)
+    return {"output": result}
